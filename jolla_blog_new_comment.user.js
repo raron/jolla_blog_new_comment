0d99af39e97f0fd9b2ae5667ecb3fd67289e4db7
Áron RADICS <raronkonektor@gmail.com>
2015-08-07 15:34:38 +0200

    do not use local time;

--- jolla_blog_new_comment.user.js
+++ jolla_blog_new_comment.user.js
@@ -8,10 +8,10 @@
 // ==/UserScript==
 
 
-var key = window.location.pathname + "---" + "lastVisit";
+var key = window.location.pathname + "---" + "latestComment";
 
-var lastVisit = GM_getValue(key, 0);
-var now = new Date().getTime();
+var latestComment = GM_getValue(key, 0);
+var newLatestComment = latestComment;
 
 var a = document.getElementById('comment-wrap');
 var b = a.getElementsByTagName('article');
@@ -21,12 +21,18 @@ for(i = 0; i < b.length; i++)
   var c = b[i].getElementsByClassName('comment_date')[0];
   var d = c.textContent.trim().replace("on ", "").replace(" at", "");
   var e = Date.parse(d);
-  if (e > lastVisit)
+
+  if (e > newLatestComment)
   {
-     var t = document.createTextNode("NEW");
-     c.parentNode.appendChild(t);
+      newLatestComment = e;
+  }
+  
+  if (e > latestComment)
+  {
+     //var t = document.createTextNode("NEW");
+     //c.parentNode.appendChild(t);
      c.parentNode.parentNode.style.backgroundColor = '#ff6666'
   }
 }
 
-GM_setValue(key, now);
+GM_setValue(key, newLatestComment);
